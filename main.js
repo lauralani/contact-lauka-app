6e627df61c2c079e184ac77f0d446be5296a01ee
lauralani <dev@lauka.space>
2021-04-18 21:48:12 +0200

    changed images to serve locally

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 function loadtextcaptcha() {
     fetch('https://api.textcaptcha.com/lauka.app.json', { mode: "no-cors" })
-        .then((response) => { console.log(response)});
-        //.then(response => response.json())
-        //.then(data => console.log(data));
+        .then((data) => {
+            console.log(data);
+        });
 }
