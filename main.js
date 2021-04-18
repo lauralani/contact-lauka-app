6d117ca4aa8090ba4242feb50fb1c99b26ce2024
lauralani <dev@lauka.space>
2021-04-18 21:39:50 +0200

    changed captcha function

--- main.js
+++ main.js
@@ -1,8 +1,6 @@
 function loadtextcaptcha() {
     fetch('https://api.textcaptcha.com/lauka.app.json', { mode: "no-cors" })
-        .then(response => response.text())
-        .then(data => {
-            console.log("test");
-            console.log(data);
-        });
+        .then((response) => { console.log(response)});
+        //.then(response => response.json())
+        //.then(data => console.log(data));
 }
